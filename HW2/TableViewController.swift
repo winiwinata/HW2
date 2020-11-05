058ee238e2c75471f5f379382255bd4b614ff263
wini <winiwinata90@gmail.com>
2020-11-05 11:45:40 +0800

    DONE !!!!! ^^

--- HW2/TableViewController.swift
+++ HW2/TableViewController.swift
@@ -4,7 +4,8 @@
 //
 //  Created by WiseLab-HP on 2020/11/3.
 //  Copyright © 2020 NDHU_CSIE. All rights reserved.
-//
+//  Wini Winata
+//  410621334
 
 import UIKit
 
@@ -12,17 +13,14 @@ class TableViewController: UITableViewController {
     
     var fruits = ["watermelon", "apple", "orange", "strawberry", "pear"]
     var prices = ["500", "100", "90", "200", "150"]
-    var images = ["fruit", "fruit", "fruit", "fruit", "fruit"]
-    var checked = Array(repeating: false, count: 5)
+    var totalPrice: Int = 0
     
+    @IBOutlet var total: UILabel!
+    
+    @IBOutlet weak var mylabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -43,72 +41,27 @@ class TableViewController: UITableViewController {
         // Configure the cell...
         cell.nameLabel.text = fruits[indexPath.row]
         cell.priceLabel.text = prices[indexPath.row]
-        cell.thumbnailImageView.image = UIImage(named:images[indexPath.row])
-        
-        if checked[indexPath.row] {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
+    
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        // Create an option menu as an action sheet
-        let optionMenu = UIAlertController(title: nil, message: "What do you want to buy?", preferredStyle: .actionSheet)
-        
-        
-        // these 6 lines of codes are only for iPads. without these, we can only run on iPhones
-        if let popoverController = optionMenu.popoverPresentationController {
-            if let cell = tableView.cellForRow(at: indexPath) {
-                popoverController.sourceView = cell
-                popoverController.sourceRect = cell.bounds
-            }
-        }
-        
-        // Check-in action
-        let checkAction = UIAlertAction(title: "Check", style: .default, handler: {
-            (action:UIAlertAction!) -> Void in
-            
-            let cell = tableView.cellForRow(at: indexPath)
-            cell?.accessoryType = .checkmark
-            self.checked[indexPath.row] = true
-            
-        })
-        optionMenu.addAction(checkAction)
-        
-        // Add Undo Check-in action
-        let unCheckAction = UIAlertAction(title: "UnCheck", style: .default, handler: {
-            (action:UIAlertAction!) -> Void in
-            
-            let cell = tableView.cellForRow(at: indexPath)
-            if self.checked[indexPath.row]{
-                cell?.accessoryType = .none
-                self.checked[indexPath.row] = false
-            }
-        })
-        optionMenu.addAction(unCheckAction)
-        
-        var priceRec = Array(prices[indexPath])
+
         //here, make an array to record the fruits chosen
         //then use loops to scan the array
         // then take the number and add it on the total prices
         
-        
-        // Add actions to the menu
-        // let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        // optionMenu.addAction(cancelAction)
-        
-        // Display the menu
-        present(optionMenu, animated: true, completion: nil)
-        
-        // Deselect a row
-        tableView.deselectRow(at: indexPath, animated: false)
-        
-        // sum the prices ///////////////////////////
-        total.text = "Total prices = " + totalPrice
-        
+        if let cell = tableView.cellForRow(at: indexPath) as? TableViewCell {
+            // storing price at itemPrice
+            if cell.accessoryType == .checkmark {
+                totalPrice -= Int(prices[indexPath.row]) ?? 0
+                cell.accessoryType = .none
+            } else {
+                totalPrice += Int(prices[indexPath.row]) ?? 0
+                cell.accessoryType = .checkmark
+            }
+        }
+        total.text = "Total Price = " + String(totalPrice)
     }
-    
 }
+
